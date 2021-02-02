8c24e55c2183fc8b8fe02f7fb4df2c2fc6bb699a
belkiz.ozbek <belkizozbek9@gmail.com>
2021-02-02 23:55:45 +0300

    isim düzeltmesi

--- src/SelamTest.java
+++ src/SelamTest.java
@@ -2,7 +2,7 @@ public class SelamTest {
 
   public static void main(String[] args) {
     Selam nesne = new Selam();
-    String cevap = nesne.selamSoyle("محسن");
+    String cevap = nesne.selamSoyle("Belkiz");
     System.out.println(cevap);
   }
 
